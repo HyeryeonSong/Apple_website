d9d3fbd1d5e244f4648745589af9535bb62f2856
Hyeryeon Song <hyeryeon_song@tmax.co.kr>
2022-07-04 17:58:42 +0900

    scroll에 따라 텍스트 투명도 조절

--- js/main.js
+++ js/main.js
@@ -4,13 +4,20 @@
   let currentScene = 0; // 현재 활성화된(눈 앞에 보고있는) 스크롤 섹션
 
   const sceneInfo = [
-  {      
-    // scroll-section-0
-    type: 'sticky',
-    heightNum: 5, // 브라우저 높이의 5배로 scrollHeight 셋팅      
+    {      
+      // scroll-section-0
+      type: 'sticky',
+      heightNum: 5, // 브라우저 높이의 5배로 scrollHeight 셋팅      
       scrollHeight: 0, // 스크롤 구간 높이
       objs: { // html 객체
-        container: document.querySelector('#scroll-section-0')
+        container: document.querySelector('#scroll-section-0'),
+        messageA: document.querySelector('#scroll-section-0 .main-message.a'),
+        messageB: document.querySelector('#scroll-section-0 .main-message.b'),
+        messageC: document.querySelector('#scroll-section-0 .main-message.c'),
+        messageD: document.querySelector('#scroll-section-0 .main-message.d'),
+      },
+      values: {
+        messageA_opacity: [0, 1],
       }
     },
     {
@@ -48,6 +55,50 @@
       sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight;
       sceneInfo[i].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`;
     }
+
+    let totalScrollHeight = 0;
+    for (let i = 0; i < sceneInfo.length; i++){
+      totalScrollHeight += sceneInfo[i].scrollHeight; // 각 scene의 scrollHeight를 더해서 넣어줌
+      if(totalScrollHeight >= yOffset) {
+        currentScene = i;
+        break;
+      }
+    }
+    document.body.setAttribute('id', `show-scene-${currentScene}`);
+  }
+
+  function calcValues(values, currentYOffset) {
+    let rv;
+    // 현재 스크롤섹션에서 스크롤된 범위를 비율로 구하기
+    let scrollRatio = currentYOffset / sceneInfo[currentScene].scrollHeight;
+    
+    rv = scrollRatio * (values[1] - values[0]) + values[0] ;
+
+    return rv;
+
+  }
+
+  // scroll될 때 애니메니션처리(텍스트위치, 투명도)
+  function playAnimation() {
+    const objs = sceneInfo[currentScene].objs;
+    const values = sceneInfo[currentScene].values;
+    const currentYOffset = yOffset - prevScrollHeight;
+
+    switch (currentScene) {      
+      case 0:
+        let messageA_opacity_in = calcValues(values.messageA_opacity, currentYOffset);
+        objs.messageA.style.opacity = messageA_opacity_in;
+        console.log(messageA_opacity_in);
+        break;
+      case 1:
+
+        console.log('1')
+        break;
+      case 2:
+        break;
+      case 3:
+        break;
+    }
   }
 
   function scrollLoop() {
@@ -63,18 +114,15 @@
     if(yOffset < prevScrollHeight) {
       currentScene --;
     }
-
-    document.body.setAttribute('id', `show-scene-${currentScene}`);
   }
 
   window.addEventListener('scroll', () => {
     yOffset = window.pageYOffset; // 수직으로 스크롤 된 px값
     scrollLoop();
+    playAnimation();
   })
   window.addEventListener('load', setLayout()); // 문서가 load되면 setLayout 실행
   window.addEventListener('resize', setLayout); // 창 크기에 따라 scrollHeight 변경
-  
-
 
 }
 )(); 
